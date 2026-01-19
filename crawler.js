de869ea20a7ac96f36ba4c8c64787ec3703f2125
joodevapp <joo.dev.app@gmail.com>
2026-01-19 21:51:31 +0900

    Update crawler.js

--- crawler.js
+++ crawler.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import cheerio from 'cheerio';
+import * as cheerio from 'cheerio';
 import fs from 'fs';
 
 const URL = 'https://www.dhlottery.co.kr/common.do?method=main';
